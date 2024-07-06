fc03c5bb6e2a3fbb0afaba859dd382f974fe2200
Mentita <a01711870@tec.mx>
2024-07-06 15:05:18 -0600

    Cree secciones, cambie el tamaño de las letras de ACM y cambie los row-start para que la alineacion sea mejor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,43 +10,50 @@ export default async function Home() {
 
   return (
     <main className="relative min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat" style={{backgroundImage: 'url(images/backgrounds/homepage.png)', minHeight: '400vh', backgroundSize: 'cover', backgroundAttachment: 'scroll'}}>
-        
-        <div className="grid grid-rows-5 grid-cols-4 items-center text-center text-white">
-          <h1 className="text-xl tracking-wider font-extrabold sm:text-[15rem] col-start-2 col-span-2 row-start-3">
-            <span className="acm text-9xl">ACM</span>
-          </h1>
-          <div className="col-start-2 col-span-2 row-start-4 items-center">
-            ¡Bienvenido a ACM! Somos un grupo estudiantil del Tecnológico de Monterrey, Campus Querétaro dedicado a la
-            programación competitiva y otros proyectos de programación. No dudes en contactarnos.
+        <section className="min-h-screen flex flex-col items-center justify-center">
+          <div className="grid grid-rows-8 grid-cols-4 items-center text-center text-white">
+            <h1 className="text-xl tracking-wide font-extrabold sm:text-[14rem] col-start-2 col-span-2 row-start-4">
+              <span className="acm text-10xl">A C M</span>
+            </h1>
+            <div className="col-start-2 col-span-2 row-start-6 items-center">
+              ¡Bienvenido a ACM! Somos un grupo estudiantil del Tecnológico de Monterrey, Campus Querétaro dedicado a la
+              programación competitiva y otros proyectos de programación. No dudes en contactarnos.
+            </div>
+            <div className="col-start-2 col-span-2 row-start-8 flex items-center justify-center space-x-6">
+              <Link href="https://www.instagram.com/acm_qro?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+              >
+                <img
+                  src="images/logos/Instagram-Icon.png"
+                />
+              </Link>
+              <Link href="/"
+              >
+                <img
+                  src="images/logos/Whatsapp-Icon.png"
+                />
+              </Link>
+              <Link href="/"
+              >
+                <img
+                  src="images/logos/Github-Icon.png"
+                />
+              </Link>
+              <Link href="/"
+              >
+                <img
+                  src="images/logos/Email-Icon.png"
+                />
+              </Link>
+            </div>
           </div>
-          <div className="col-start-2 col-span-2 row-start-5 flex items-center justify-center space-x-4">
-            <Link href="https://www.instagram.com/acm_qro?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-            >
-              <img
-                src="images/logos/Instagram-Icon.png"
-              />
-            </Link>
-            <Link href="/"
-            >
-              <img
-                src="images/logos/Whatsapp-Icon.png"
-              />
-            </Link>
-            <Link href="/"
-            >
-              <img
-                src="images/logos/Github-Icon.png"
-              />
-            </Link>
-            <Link href="/"
-            >
-              <img
-                src="images/logos/Email-Icon.png"
-              />
-            </Link>
+        </section>
+        <section className="min-h-screen flex flex-col items-center justify-center">
+          <div className="grid grid-rows-5 grid-cols-4 items-center text-center text-white">
+            <h2>
+              Informacion Genereal
+            </h2>
           </div>
-        </div>
-  
+        </section>
     </main>
   );
 }
