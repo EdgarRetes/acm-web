67a841b5d73a46f0f43d52329e8e21f8ba23da9c
EdgarRetes <edgar.retes24@gmail.com>
2024-07-13 17:11:29 -0600

    Revert "[FIX] collaborators layout"
    This reverts commit a276b4efd62f073737f1a44819410b0f675fcab3.

---
+++ src/app/collaborators-page/page.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+export default async function Collaborators() {
+    return(
+        <main className=" relative min-h-screen items-center justify-center bg-gradient-to-b from-blue-950 to-slate-900">
+            <section className="min-h-screen items-center justify-center">
+                <div className="grid grid-cols-7 grid-rows-8 font-mono">
+                    <h1 className="text-sky-600 row-start-4 col-start-4 col-span-2 text-6xl font-bold">
+                        Nombre
+                    </h1>
+                    <h1 className="text-white row-start-5 col-start-4 col-span-2 text-6xl font-bold">
+                        Apellido
+                    </h1>
+                    <div className="text-white row-start-6 col-start-4 col-span-3 py-1">
+                        !Felicidades ____ por ser el colaborador del mes! Se reconoce tu trabajo
+                        en los proyectos y eventos para fomentar la pasión por la programación en nuestra comunidad. 
+                    </div>
+                    <div className="text-white row-start-7 col-start-4 col-span-3 font-bold text-3xl py-6">
+                        Carrera
+                    </div>
+                    <div className="text-white row-start-8 col-start-4 col-span-3 font-bold text-3xl">
+                        Semestre
+                    </div>
+                </div>
+            </section>
+            <section className="min-h-screen items-center justify-center text-center">
+                <div className="grid grid-cols-10 grid-rows-12 font-mono space-y-5" style={{ gridTemplateRows: '50px auto' }}>
+                    <h1 className="text-white col-start-2 col-span-5 text-4xl font-bold border-x-blue-700 border-l-8 border-spacing-12">
+                    <span className="invisible">
+                        ..
+                    </span>
+                        Conoce a nuestros colaboradores
+                    </h1>
+                    <div className="col-start-2">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                    </div>
+                    <div className="col-start-3">
+                        Hola
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
