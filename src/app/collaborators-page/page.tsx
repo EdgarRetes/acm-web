ef68be7a0a5324399f06e651643c855520216977
EdgarRetes <edgar.retes24@gmail.com>
2024-07-13 17:19:00 -0600

    Revert "[FIX] collaborators layout"

--- src/app/collaborators-page/page.tsx
+++ src/app/collaborators-page/page.tsx
@@ -25,24 +25,186 @@ export default async function Collaborators() {
                     <div className="text-white row-start-8 col-start-4 col-span-3 font-bold text-3xl">
                         Semestre
                     </div>
+                    <div className="flex flex-col items-center justify-center col-start-2 col-span-2 row-start-4 row-span-3">
+                        <img
+                        src="images/temp/circle-temp.png"
+                        className="size-60"
+                        />
+                    </div>
                 </div>
             </section>
             <section className="min-h-screen items-center justify-center text-center">
                 <div className="grid grid-cols-10 grid-rows-12 font-mono space-y-5" style={{ gridTemplateRows: '50px auto' }}>
-                    <h1 className="text-white col-start-2 col-span-5 text-4xl font-bold border-x-blue-700 border-l-8 border-spacing-12">
+                    <h1 className="text-white row-start-1 col-start-2 col-span-5 text-4xl font-bold border-x-blue-700 border-l-8 border-spacing-12">
                     <span className="invisible">
                         ..
                     </span>
                         Conoce a nuestros colaboradores
                     </h1>
-                    <div className="col-start-2">
+
+                    {/* Primera fila */}
+                    <div className="flex flex-col items-center justify-center row-start-2 col-start-2 p-4 hover:bg-blue-900 rounded-full">
                         <h2 className="text-sky-500 font-bold">
                             Nombre
                         </h2>
                         <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
                     </div>
-                    <div className="col-start-3">
-                        Hola
+                    <div className="flex flex-col items-center justify-center row-start-2 col-start-3 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-2 col-start-4 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-2 col-start-5 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-2 col-start-6 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-2 col-start-7 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-2 col-start-8 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-2 col-start-9 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+
+                    {/* Segunda fila */}
+                    <div className="flex flex-col items-center justify-center row-start-3 col-start-2 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center row-start-3 col-start-3 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-3 col-start-4 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-3 col-start-5 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-3 col-start-6 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-3 col-start-7 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-3 col-start-8 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col items-center justify-center row-start-3 col-start-9 p-4 hover:bg-blue-900 rounded-full">
+                        <h2 className="text-sky-500 font-bold">
+                            Nombre
+                        </h2>
+                        <img src="images/temp/circle-temp.png"/>
+                        <h3 className="text-white">
+                            <p>Carrera</p>
+                            <p>Semestre</p>
+                        </h3>
                     </div>
                 </div>
             </section>
