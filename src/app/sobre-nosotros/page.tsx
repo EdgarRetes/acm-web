9d2609774b8320952e07fbb6aba098b9a4c670c9
SALV <a01707122@tec.mx>
2024-07-12 22:41:45 -0600

    [CHANGE] image-position

--- src/app/sobre-nosotros/page.tsx
+++ src/app/sobre-nosotros/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const SobreNosotros = () => {
     return (
-        <main className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#020F46] to-[#0E152A] text-white">
+        <main className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#020F46] to-[#0E152A] text-white pt-[10rem]">
             <section className="flex flex-col items-center justify-center w-full">
-                <div className="grid grid-rows-6 grid-cols-7 items-center text-center text-white">
+                <div className="grid grid-rows-5 grid-cols-7 items-center text-center text-white pb-0">
                     <h1 className="text-[65px] col-start-2 col-span-6 row-start-1 row-span-2 mt-35">
                         <span className="sobre nosotros">
                             SOBRE NOSOTROS
@@ -19,11 +19,11 @@ const SobreNosotros = () => {
                         />
                     </div>
 
-                    <div className="flex flex-col items-center col-start-2 col-span-2 row-start-3 row-span-3 mt-9">
+                    <div className="flex flex-col items-center col-start-2 col-span-2 row-start-3 row-span-3 mt-9 mb-0">
                         <span className="text-[32px] mb-2">
                             MISIÓN
                         </span>
-                        <div className="bg-[#39275E] p-8 rounded-lg mt-2 w-[490px] h-[190px] flex items-center justify-center text-justify text-[15px]">
+                        <div className="bg-[#39275E] p-8 rounded-lg mt-2 w-[490px] h-[190px] flex items-center justify-center text-justify text-[15px] ">
                             <p>
                                 Ser un capítulo enfocado a promover y difundir las ciencias computacionales e 
                                 inculcar el interés por la programación competitiva. Uniendo a la comunidad 
@@ -45,16 +45,16 @@ const SobreNosotros = () => {
                         </div>
                     </div>
 
-                    <div className="flex items-center justify-center col-start-4  row-start-4 row-span-6">
+                </div>
+                    <div className="flex items-center justify-center col-start-4  row-start-4 row-span-6 -mt-[5.5rem]">
                         <img
                             src="images/ACM Team.png"
-                            className="w-[500px] h-auto max-w-none"
+                            className="w-[500px] h-auto max-w-none "
                             alt="Logo Sobre Nosotros"
                         />
                     </div>
-
-                </div>
             </section>
+
         </main>
     );
 }
