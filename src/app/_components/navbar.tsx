7172642c0868fc6cac1ecd7b5b0e90030b6ef9f8
Santiago Alducin Villaseñor <145412063+SALV19@users.noreply.github.com>
2024-07-23 11:38:14 -0600

    Accidental miss-redirection on contact-page
    I guess I pressed crt+z one to many times

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar({
               <Link href="/collaborators-page" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 Colaboradores
               </Link>
-              <Link href="/page2" className="text-gray-300 hover:bg-gray-700 hover:bg-opacity-40 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              <Link href="/Contact" className="text-gray-300 hover:bg-gray-700 hover:bg-opacity-40 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 Contacto
               </Link>
             </div>
